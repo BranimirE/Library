ebe0136b9210981ce9160a5f9a1ae7c5d4196d3b
Branimir <brani.espinoza@gmail.com>
2014-08-18 14:23:09 -0400

    Create template.cpp
    Mi nueva plantilla para contests

---
+++ template.cpp
@@ -0,0 +1,15 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+#define DBG(X) cerr << #X << " = " << X << endl;
+#define pb push_back
+#define mp make_pair
+#define sz(x) ((int)(x).size())
+#define all(c) (c).begin(),(c).end()
+#define forn(i, n) for (int i = 0; i < (n); i++)
+
+int main() {
+  
+  return 0;
+}
